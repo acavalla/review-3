dd6a7b532a29cdca02732bfc2152bd9869d3009b
Anna Cavalla <Anna.Cavalla@gmail.com>
2021-03-16 17:06:51 +0000

    Refactored to use more descriptive variable names

--- lib/spellcheck.rb
+++ lib/spellcheck.rb
@@ -1,32 +1,32 @@
 class Spellcheck
-  def check(string, array)
-    dictionary_verify(string, array)
-    join_new_string(string, new_string)
+  def check(sentence, dict)
+    dictionary_verify(sentence, dict)
+    join_new_string(sentence, new_sentence)
   end
 
   private
-  attr_reader :new_string
+  attr_reader :new_sentence
 
-  def dictionary_verify(string, array)
-    @new_string = []
-    string.split(" ").each do |substring|
-      word_verify(substring, array)
+  def dictionary_verify(sentence, dict)
+    @new_sentence = []
+    sentence.split(" ").each do |substring|
+      word_verify(substring, dict)
     end
   end
 
-  def word_verify(substring, array)
-    if array.include?(substring.downcase)
-      new_string << substring
+  def word_verify(substring, dict)
+    if dict.include?(substring.downcase)
+      new_sentence << substring
     else
-      new_string << "~" + substring + "~"
+      new_sentence << "~" + substring + "~"
     end
   end
 
-  def join_new_string(string, new_string)
-    if string.split("")[string.split(" ")[0].length+1] == " "
-      new_string.join("  ")
+  def join_new_string(sentence, new_sentence)
+    if sentence.split("")[sentence.split(" ")[0].length+1] == " "
+      new_sentence.join("  ")
     else
-      new_string.join(" ")
+      new_sentence.join(" ")
     end
   end
 end
