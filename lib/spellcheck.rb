65669225525fddff5d77b9f867c90ca3fe50f627
Anna Cavalla <Anna.Cavalla@gmail.com>
2021-03-16 16:19:24 +0000

    Can handle individual words whether correct or incorrect, and strings of multiple words if correct

--- lib/spellcheck.rb
+++ lib/spellcheck.rb
@@ -1,9 +1,13 @@
 class Spellcheck
   def check(string, array)
-    if array.include?(string)
-      string
-    else
-      "~" + string + "~"
+    new_string = []
+    string.split(" ").each do |string|
+      if array.include?(string)
+        new_string << string
+      else
+        new_string << "~" + string + "~"
+      end
     end
+    new_string.join(" ")
   end
 end
