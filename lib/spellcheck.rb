4a270b46c3bc7006959e8d28c041f594b63ffe56
Anna Cavalla <Anna.Cavalla@gmail.com>
2021-03-16 16:25:46 +0000

    Refactored

--- lib/spellcheck.rb
+++ lib/spellcheck.rb
@@ -1,5 +1,11 @@
 class Spellcheck
   def check(string, array)
+    dictionary_verify(string, array).join(" ")
+  end
+
+  private
+
+  def dictionary_verify(string, array)
     new_string = []
     string.split(" ").each do |string|
       if array.include?(string)
@@ -8,6 +14,6 @@ class Spellcheck
         new_string << "~" + string + "~"
       end
     end
-    new_string.join(" ")
+    new_string
   end
 end
